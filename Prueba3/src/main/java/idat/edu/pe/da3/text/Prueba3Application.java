1cc3602a9ca28f810d8318dd59735097dd7d947e
User <User@192.168.1.3>
2022-10-12 17:40:34 -0500

    Mi primer comit al repositorio github

---
+++ Prueba3/src/main/java/idat/edu/pe/da3/text/Prueba3Application.java
@@ -0,0 +1,13 @@
+package idat.edu.pe.da3.text;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prueba3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prueba3Application.class, args);
+	}
+
+}
